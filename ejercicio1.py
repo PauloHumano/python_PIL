829b71f76f83c66e33f31577532b8fee364a3f7e
¨Paulo <paulohumano@gmail.com>
2022-09-02 09:04:28 -0300

    listo

--- ejercicio1.py
+++ ejercicio1.py
@@ -2,16 +2,6 @@
 a = float(2.4)
 b = float(-3.21)
 c = float(47.8)
-# generar diccionario
-d = [['a',a],
-    ['b',b],
-    ['c',c]]
 # dar formato a print
-print('{:<5} {:<5} {:<5}'.format('a','b','c'))
-def new_func(v):
-    a, b, c = v
-    return a,b,c
-
-for v in d:
-    a, b, c = new_func(v)
-print('{:<5} {:<5} {:<5}'.format(a, b, c))
+print("a \t", "b \t", "c")
+print(a , end ="\t"), print(b ,end ="\t"), print(c)
