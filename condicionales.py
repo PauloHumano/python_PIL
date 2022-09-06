2321f4c5628345fd1d75b0d7b8960c0b08d8738d
¨Paulo <paulohumano@gmail.com>
2022-09-06 19:57:55 -0300

    clase_3

---
+++ condicionales.py
@@ -0,0 +1,19 @@
+# Condicionales if
+a = 5
+
+# elif
+if a == 3
+    print('A = 3')
+elif a==4
+    print('A = 4')
+elif a==5
+    print('A = 5')
+else:
+    print('A no es igual a nada')
+    
+
+
+    if a > c:
+        print('C>A& C>B')
+else:
+    print('ingreso al else')
