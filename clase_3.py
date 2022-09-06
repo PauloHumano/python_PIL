de9b87671ff2e0b722d027ba6b445e32b5a67f50
¨Paulo <paulohumano@gmail.com>
2022-09-06 19:58:10 -0300

    CLASE3

---
+++ clase_3.py
@@ -0,0 +1,13 @@
+# condicionales de python
+a = 2
+b = 2
+r = a + b
+print(type(r))
+print(r)
+
+if a < b :
+    print('A es menor es menor a B: ')
+else:
+    print('B es menor a A')
+print('final del codigo')
+    
