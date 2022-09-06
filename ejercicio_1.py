127d03a586bdeec5557027e7a093d5c2c2b76d65
¨Paulo <paulohumano@gmail.com>
2022-09-06 09:42:43 -0300

    otra cororeccion ej 1

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -4,4 +4,6 @@ b = float(-3.21)
 c = float(47.8)
 # dar formato a print
 print("a \t", "b \t", "c")
-print(a , end ="\t"), print(b ,end ="\t"), print(c)
+print(a, end="\t")
+print(b, end="\t")
+print(c)
