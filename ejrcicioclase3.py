f486ccbf92cc9407e963bef0f2fa24968aa658e7
¨Paulo <paulohumano@gmail.com>
2022-09-06 22:49:27 -0300

    ejercicio de clasew3

---
+++ ejrcicioclase3.py
@@ -0,0 +1,28 @@
+# MENU con while true:
+
+opcion = 0
+while True:
+    print('\n')
+    print('\t MENU\n')
+    print('\t 1.deposito')
+    print('\t 2.extraccion')
+    print('\t 3.transferencia')
+    print('\t 4.salir')
+    print('ingrese opcion', end='\t')
+    opcion = int(input())
+    if opcion > 4 :
+        print('\n vuelva a intentar')
+    elif opcion == 0 :
+        print('\n vuelva a intentar')
+    elif opcion == 1:
+        print('ingrese monto deposito', end=' ')
+        monto = input()
+    elif opcion == 2:
+        print('ingrese monto extraccion', end=' ')
+        monto = input()
+    elif opcion == 3:
+        print('ingrese monto transferencia', end=' ')
+        monto = input()
+    elif opcion == 4:
+        print('\t \t adios')
+        break
